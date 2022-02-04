98ceb44a69bd6149a0ad32cf247090fa666ec9df
a <a@a.com>
2022-02-05 00:18:44 +0900

    backup

--- src/main/kotlin/com/fivemin/generator/presentation/CorsFilter.kt
+++ src/main/kotlin/com/fivemin/generator/presentation/CorsFilter.kt
@@ -1,9 +1,12 @@
 package com.fivemin.generator.presentation
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 import org.springframework.web.server.ServerWebExchange
 import org.springframework.web.server.WebFilter
 import org.springframework.web.server.WebFilterChain
@@ -18,4 +21,18 @@ class WebConfig: WebMvcConfigurer {
     fun addCorsMapping(registry: CorsRegistry) {
         registry.addMapping("/**")
     }
+}
+
+@Bean
+fun corsConfigurationSource() {
+    val config = CorsConfiguration()
+
+    config.allowedOriginPatterns = listOf("*")
+    config.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
+    config.allowedHeaders = listOf("Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
+        "Access-Control-Request-Method", "Access-Control-Request-Headers", "Origin",
+        "Cache-Control", "Content-Type")
+
+    val source = UrlBasedCorsConfigurationSource()
+    source.registerCorsConfiguration("/**", config)
 }
