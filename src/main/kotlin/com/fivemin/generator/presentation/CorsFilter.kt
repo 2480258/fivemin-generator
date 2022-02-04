1ca1355e0b1141345e757c46e060ad6d6568a3a3
a <a@a.com>
2022-02-05 00:04:53 +0900

    backup

---
+++ src/main/kotlin/com/fivemin/generator/presentation/CorsFilter.kt
@@ -0,0 +1,21 @@
+package com.fivemin.generator.presentation
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Component
+import org.springframework.web.server.ServerWebExchange
+import org.springframework.web.server.WebFilter
+import org.springframework.web.server.WebFilterChain
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+@EnableWebMvc
+class WebConfig: WebMvcConfigurer {
+    @Override
+    fun addCorsMapping(registry: CorsRegistry) {
+        registry.addMapping("*")
+    }
+}
